76bc20bdd71021f1708d511d239bdd465fdef351
Nikita Timoshin newc <nikitatimoshinpost@gmail.com>
2025-02-24 23:41:11 +0300

    page object не работает 2

--- data/constants.py
+++ data/constants.py
@@ -1,4 +1,4 @@
 URL = 'https://dev.smorodina.ru/'
-SELLER_LOGIN = 'nikitatimoshinpost@gmail.com'
-SELLER_PASSWORD = 'Nik123'
-EXPECTED_URL_AFTER_LOGIN = 'https://dev.smorodina.ru/partner/summary'
+SELLER_LOGIN1 = 'nikitatimoshinpost@gmail.com'
+SELLER_PASSWORD1 = 'Nik123'
+EXPECTED_URL_AFTER_LOGIN = 'dev.smorodina.ru/partner/summary'
--- tests/Seller/auth/test_seller_auth.py
+++ tests/Seller/auth/test_seller_auth.py
@@ -1,10 +1,9 @@
 import pytest
 import allure
 from fixtures.all import intercept_requests
-from data.constants import SELLER_LOGIN, SELLER_PASSWORD, URL, EXPECTED_URL_AFTER_LOGIN 
 from pages.main_page import MainPage
 from data.assertions import Assertions
-from data.constants import EXPECTED_URL_AFTER_LOGIN, URL
+from data.constants import EXPECTED_URL_AFTER_LOGIN, URL, SELLER_LOGIN1, SELLER_PASSWORD1
 
 @allure.feature('Продавец')
 @allure.story('Авторизация продавца')
@@ -18,8 +17,8 @@ def test_auth1(page, intercept_requests):
     main_page.click_search_tours()  # Нажимаем на кнопку "Искать туры"
     main_page.click_login()  # Нажимаем на кнопку "Войти"
     main_page.click_tour_organizer()  # Нажимаем на "Организатор туров"
-    main_page.fill_email(SELLER_LOGIN)  # Заполняем email
-    main_page.fill_password(SELLER_PASSWORD)  # Заполняем пароль
+    main_page.fill_email(SELLER_LOGIN1)  # Заполняем email
+    main_page.fill_password(SELLER_PASSWORD1)  # Заполняем пароль
     main_page.submit_login()  # Нажимаем кнопку "Войти"
 
     # Проверяем конечный URL после логина
