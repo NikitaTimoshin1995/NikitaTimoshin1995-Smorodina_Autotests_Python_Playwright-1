c7a9d97171b6675752cd101e3eed39977d0a64c3
Nikita Timoshin newc <nikitatimoshinpost@gmail.com>
2025-02-26 22:21:53 +0300

    URL в переменные, стенды

--- data/constants.py
+++ data/constants.py
@@ -3,6 +3,6 @@ URL = 'https://dev.smorodina.ru/'
 #Авторизация продавца
 SELLER_LOGIN1 = 'nikitatimoshinpost@gmail.com'  
 SELLER_PASSWORD1 = 'Nik123'
-EXPECTED_URL_AFTER_LOGIN_SELLER = 'https://dev.smorodina.ru/partner/summary'
+EXPECTED_URL_AFTER_LOGIN_SELLER = f"{URL}partner/summary"
 SELLER_AUTH_ERROR1 = 'Поле логин является обязательным для заполнения.'
 SELLER_AUTH_ERROR2 = 'Поле пароль является обязательным для заполнения.'
