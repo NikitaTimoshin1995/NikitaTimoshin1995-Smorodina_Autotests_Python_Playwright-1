cdfca1eefc9f260c8eaaa64dcef1275750162e25
Nikita Timoshin newc <nikitatimoshinpost@gmail.com>
2025-02-27 23:26:50 +0300

    Два теста с логами запросов

--- fixtures/all.py
+++ fixtures/all.py
@@ -1,5 +1,6 @@
 import pytest
 from playwright.sync_api import Page
+import time
 
 @pytest.fixture
 def intercept_requests(page: Page):
@@ -13,6 +14,10 @@ def intercept_requests(page: Page):
         # Проверяем, начинается ли URL с указанного префикса
         if request.url.startswith("https://ff.kis.v2.scr.kaspersky-labs.com/"):
             return  # Пропускаем этот запрос
+
+        # Записываем время и URL запроса
+        log_time = time.strftime('%H:%M:%S')
+        print(f"{log_time} - Запрос: {request.method} {request.url}")  # Логируем метод и URL
 
         requests.append(request)
 
